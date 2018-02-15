d28e23f40d7f0f2093560f7880d3dbfb78facee0
Serena Hathi <hathi.serena@gmail.com>
2018-02-15 15:49:23 +0000

    Exercise 13 continued

--- lib/link.rb
+++ lib/link.rb
@@ -3,29 +3,26 @@ require_relative 'database_connection'
 
 class Link
 
-  attr_reader :url
+  attr_reader :url, :id
 
-  def initialize(url)
+  def initialize(id, url)
     @url = url
+    @id = id
   end
 
   def self.all
-    # connection = PG.connect dbname: 'bookmark_manager_' + ENV['RACK_ENV']
-    # result = connection.exec 'SELECT * FROM links'
+   
     result = DatabaseConnection.query('SELECT * FROM links')
-     # result.map { |row| row['url'] }
-     result.map { |link| Link.new(link['url']) }
+     result.map { |link| Link.new(link['id'],link['url']) }
   end
 
   def self.add(link)
     fail "Invalid link given" if url?(link)
     DatabaseConnection.query("INSERT INTO links (url) VALUES('#{link}')")
-    # connection = PG.connect dbname: 'bookmark_manager_' + ENV['RACK_ENV']
-    # connection.exec "INSERT INTO links (url) VALUES('#{link}')"
+    
   end
 
   def self.url?(link)
     link !~ /^http:\/\/www\..+\..+/
-    # ^ = at the start of the url
   end
 end
